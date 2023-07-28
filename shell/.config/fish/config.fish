a3718c80710b9bf8537989f61ac99f6cb588978b
Dean Li <deantvv@gmail.com>
2023-07-29 00:48:02 +0800

    fish config

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -8,7 +8,7 @@ set -x LANG en_US.UTF-8
 
 if status is-interactive
 	if ! set -q TMUX
-		exec tmux
+		exec tmux new-session -A -s main
 	end
 end
 
@@ -23,7 +23,26 @@ else
 	abbr -a lll 'ls -la'
 end
 
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
+
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
 # Enable pyenv
 pyenv init - | source
+
